d7d3d06d3e52d73216c8b7ea69c63e0769d76895
Vladislav Luchnikov <whatislov@mail.ru>
2024-03-17 16:44:14 +0300

    Добавлен класс для парсинга вакансий

---
+++ FileHandler.py
@@ -0,0 +1,34 @@
+import csv
+from abc import ABC, abstractmethod
+
+
+# абстрактный класс для обработки файловых структур
+class AbstractFileHandler(ABC):
+    @abstractmethod
+    def save_file(self, data):
+        pass
+
+    @abstractmethod
+    def read_file(self):
+        pass
+
+
+# класс по работе с CSV файлами
+class CSVHandler(AbstractFileHandler):
+    def save_file(self, data):
+        with open('data.csv', 'a', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile, delimiter=',')
+            for row in data:
+                try:
+                    csvwriter.writerow((row['id'],
+                                        row['name'],
+                                        row['area']['name'],
+                                        row['employer']['name'],
+                                        row['salary'],
+                                        row['snippet']['requirement'],
+                                        row['experience']['name']))
+                except:
+                    print('Ошибка записи строки')
+
+    def read_file(self):
+        pass
